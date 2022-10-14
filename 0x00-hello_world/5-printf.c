9854e532e25d516cbd98201b426d85b7cf505b21
Wahab300 <anasabdul200.com>
2022-10-14 09:46:08 -0700

    5p

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
  * main -Main function
  *
- *
  * Return: return 0 everytime
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
