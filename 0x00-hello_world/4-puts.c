56bcad398b7f7ea80bc521d74d4e2accfcd5bcde
Wahab300 <anasabdul200.com>
2022-10-14 09:09:51 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Main function of project
+ *
+ *
+ * Return: return 0 everytime
+ *
+ */
+int main(void)
+{
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual pizzle");
+	puts(displayed);
+	return (0);
+}
